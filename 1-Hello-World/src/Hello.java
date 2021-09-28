e6b8f60afeebef74b9f3590bed98e2f2e9ad57d1
Himank Suiwala <himanksuiwala@gmail.com>
2021-09-28 17:21:33 +0530

    Traditional 'Hello World' program in Java

---
+++ 1-Hello-World/src/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+
+    public static void main(String[] args){
+        System.out.println("Hello World"+", Himank");
+
+        int myFirstNumber = 5+10*100;
+
+        System.out.println(myFirstNumber);
+    }
+}
